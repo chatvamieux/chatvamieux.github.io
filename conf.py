510ea401f3039a149b16a2525ea7c4ffa6e8ae91
ouroumov <Etienne Papegnies>
2023-10-01 12:00:48 +0200

    Nikola auto commit.
    Nikola version: 8.2.4

--- conf.py
+++ conf.py
@@ -145,18 +145,21 @@ NAVIGATION_LINKS = {
         ("/archive.html", "Archives"),
         ("/categories/", "Étiquettes"),
         ("/rss.xml", "Flux RSS"),
+	("/blog", "Blog"),
     ),
 
     "en": (
         ("/en/archive.html", "Archive"),
         ("/en/categories/", "Tags"),
         ("/en/rss.xml", "RSS feed"),
+	("/blog", "Blog"),
     ),
 
     "es": (
         ("/es/archive.html", "Archivo"),
         ("/es/categories/", "Etiquetas"),
         ("/es/rss.xml", "Canal RSS"),
+	("/blog", "Blog"),
     ),
 }
 
